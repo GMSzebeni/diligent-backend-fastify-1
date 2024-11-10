0eacfec4d172e690922c52f340a3f5a6aa1ac54d
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-10 19:59:08 +0100

    add readWithKinds query and toEntityWithKind() method to create a PetWithKind entity after query

--- src/repository/pet.repository.ts
+++ src/repository/pet.repository.ts
@@ -1,5 +1,5 @@
 import { DbClient } from "../db";
-import { Pet, PetToCreate } from "../entity/pet.type";
+import { Pet, PetToCreate, PetWithKind } from "../entity/pet.type";
 
 export class PetRepository {
   private readonly client;
@@ -18,6 +18,17 @@ export class PetRepository {
     }
   }
 
+  private toEntityWithKind(record: any): PetWithKind {
+    const { id, name, age, weight_in_kg, kind } = record;
+    return {
+      id,
+      name,
+      age,
+      weightInKg: parseFloat(weight_in_kg),
+      kind
+    }
+  }
+
   async read({ limit, offset }: { limit?: number, offset?: number } = {}) {
     const sql = 'SELECT id, name, age, weight_in_kg FROM pet LIMIT $1 OFFSET $2;'
     const rows = await this.client.query(sql, [limit, offset]) as Array<unknown>;
@@ -33,5 +44,11 @@ export class PetRepository {
     `
     const rows  = await this.client.query(sql, [name, age, weightInKg]) as Array<unknown>
     return rows.map(this.toEntity)[0];
+  }
+
+  async readWithKinds({ limit, offset }: {limit?: number, offset?: number} = {}) {
+    const sql = 'SELECT pet.id, pet.name, pet.age, pet.weight_in_kg, pet_kind.name kind FROM pet JOIN pet_kind ON pet.kind_id = pet_kind.id LIMIT $1 OFFSET $2';
+    const rows = await this.client.query(sql, [limit, offset]) as Array<unknown>;
+    return rows.map(this.toEntityWithKind);
   }
 }
