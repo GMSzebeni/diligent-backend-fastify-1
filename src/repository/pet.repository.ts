56f5ce58b701fccb847a569e6e28d8c2cd1108ac
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-11 10:42:51 +0100

    add createWithKind to add kind directly at creation

--- src/repository/pet.repository.ts
+++ src/repository/pet.repository.ts
@@ -47,16 +47,29 @@ export class PetRepository {
   async create(pet: PetToCreate) {
     const {name, age, weightInKg} = pet;
     const sql = `
-      INSERT INTO pet (name, age, weight_in_kg) VALUES 
-        ($1, $2, $3) 
-      RETURNING *
+      INSERT INTO pet (name, age, weight_in_kg)
+      VALUES ($1, $2, $3)
     `
     const rows  = await this.client.query(sql, [name, age, weightInKg]) as Array<unknown>
     return rows.map(this.toEntity)[0];
   }
 
+  async createWithKind(pet: PetToCreate) {
+    const {name, age, weightInKg, kindId} = pet;
+    const sql = `
+      INSERT INTO pet (name, age, weight_in_kg, kind_id)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id, name, age, weight_in_kg, 
+      (SELECT name kind FROM pet_kind WHERE id = kind_id)
+    `
+    const rows  = await this.client.query(sql, [name, age, weightInKg, kindId]) as Array<unknown>
+    return rows.map(this.toEntityWithKind)[0];
+  }
+
   async readWithKinds({ limit, offset }: {limit?: number, offset?: number} = {}) {
-    const sql = 'SELECT pet.id, pet.name, pet.age, pet.weight_in_kg, pet_kind.name kind FROM pet JOIN pet_kind ON pet.kind_id = pet_kind.id LIMIT $1 OFFSET $2';
+    const sql = `SELECT pet.id, pet.name, pet.age, pet.weight_in_kg, pet_kind.name kind 
+                FROM pet JOIN pet_kind ON pet.kind_id = pet_kind.id 
+                LIMIT $1 OFFSET $2`;
     const rows = await this.client.query(sql, [limit, offset]) as Array<unknown>;
     return rows.map(this.toEntityWithKind);
   }
