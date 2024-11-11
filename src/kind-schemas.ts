f7076d4cb0fec4e2ac1ba1bdcaa10ba200f4cbb4
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-11 09:13:38 +0100

    update getKindSchema syntax

--- src/kind-schemas.ts
+++ src/kind-schemas.ts
@@ -1,16 +1,18 @@
 export const getKindsSchema = {
-  200: {
-    type: 'array',
-    items: {
-      type: 'object',
-      properties: {
-        id: {
-          type: 'number'
-        },
-        name: {
-          type: 'string'
+  response: {
+    200: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'number'
+          },
+          name: {
+            type: 'string'
+          }
         }
       }
     }
   }
-} as const;
+};
