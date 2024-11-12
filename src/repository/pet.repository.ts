25e45241e86e92f0e31f24339290c19a17be5264
GMSzebeni <gabriella.szebeni@gmail.com>
2024-11-12 12:09:16 +0100

    add updateWithKind written so that it partially updates entity

--- src/repository/pet.repository.ts
+++ src/repository/pet.repository.ts
@@ -1,6 +1,6 @@
 import { DbClient } from "../db";
 import { Kind } from "../entity/kind.type";
-import { Pet, PetToCreate, PetWithKind } from "../entity/pet.type";
+import { Pet, PetToCreate, PetToUpdate, PetWithKind } from "../entity/pet.type";
 
 export class PetRepository {
   private readonly client;
@@ -67,11 +67,34 @@ export class PetRepository {
   }
 
   async readWithKinds({ limit, offset }: {limit?: number, offset?: number} = {}) {
-    const sql = `SELECT pet.id, pet.name, pet.age, pet.weight_in_kg, pet_kind.name kind 
-                FROM pet JOIN pet_kind ON pet.kind_id = pet_kind.id 
-                LIMIT $1 OFFSET $2`;
+    const sql = 
+      `SELECT pet.id, pet.name, pet.age, pet.weight_in_kg, pet_kind.name kind 
+      FROM pet JOIN pet_kind ON pet.kind_id = pet_kind.id 
+      LIMIT $1 OFFSET $2`;
     const rows = await this.client.query(sql, [limit, offset]) as Array<unknown>;
     return rows.map(this.toEntityWithKind);
+  }
+
+  async updateWithKind(id: number, pet: PetToUpdate) {
+    const { name, age, weightInKg, kindId } = pet;
+
+    const sql = 
+      `UPDATE pet 
+      SET 
+        name = COALESCE($2, name), 
+        age = COALESCE($3, age), 
+        weight_in_kg = COALESCE($4, weight_in_kg), 
+        kind_id = COALESCE($5, kind_id)
+      WHERE id = $1
+      RETURNING id, name, age, weight_in_kg, (SELECT name kind FROM pet_kind WHERE id = kind_id)`
+    const rows = await this.client.query(sql, [
+      id, 
+      name ?? null, 
+      age ?? null, 
+      weightInKg ?? null, 
+      kindId ?? null
+    ]) as Array<unknown>;
+    return rows.map(this.toEntityWithKind)[0];
   }
 
   async readKinds() {
